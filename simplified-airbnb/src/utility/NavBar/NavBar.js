bb246d25c5a0e0efbd2fee3b9839320d2b70105d
Ramin Radmand <ramin.radmand@gmail.com>
2020-11-22 13:36:53 -0700

    created nav bar w/styling

--- simplified-airbnb/src/utility/NavBar/NavBar.js
+++ simplified-airbnb/src/utility/NavBar/NavBar.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react'
 import './NavBar.css'
+import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
     render(){
         return(
-            <h1>nav</h1>
+            <div className='container-fluid nav'>
+                <div className='row'>
+                    <nav className='transparent'>
+                        <div className='nav-wrapper'>
+                            <ul id='nav-mobile' className='right'>
+                                <li><Link to='/'>English US</Link></li>
+                                <li><Link to='/'>$ USD</Link></li>
+                                <li><Link to='/'>Become a host</Link></li>
+                                <li><Link to='/'>Help</Link></li>
+                                <li><Link to='/'>Sign up</Link></li>
+                                <li><Link to='/'>Login</Link></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
         )
     }
 }
