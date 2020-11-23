8f697db30ba956e7be005cf880b6b682e33bd050
Ramin Radmand <ramin.radmand@gmail.com>
2020-11-22 18:14:02 -0700

    added spinner for slow loading time

--- simplified-airbnb/src/utility/Spinner/Spinner.js
+++ simplified-airbnb/src/utility/Spinner/Spinner.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import{faSpinner} from '@fortawesome/free-solid-svg-icons'
+import {library} from '@fortawesome/fontawesome-svg-core'
 import './Spinner.css'
+
+library.add(faSpinner)
 
 class Spinner extends Component {
 
     render() {
         return (
-            <div className = 'spinner'>
-             <h1>Spinner</h1>
+            <div className = 'spinner-wrapper'>
+                <FontAwesomeIcon icon='spinner' size='6px' spin/>
             </div>
         )
     }
