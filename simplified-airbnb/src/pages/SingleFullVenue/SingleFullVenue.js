7ee28c0b6b9a106178a21ed74766f6e5f29ae94b
Ramin Radmand <ramin.radmand@gmail.com>
2020-12-22 22:30:03 -0700

    made new component for the venue items when clicked

---
+++ simplified-airbnb/src/pages/SingleFullVenue/SingleFullVenue.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class SingleFullVenue extends Component {
+
+    render () {
+        return (
+            null 
+        )
+    }
+}
+
+export default SingleFullVenue
