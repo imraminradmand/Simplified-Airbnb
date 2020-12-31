15d8b96f3f161c1e4f84da3dbae4bf1d6a667006
Ramin Radmand <ramin.radmand@gmail.com>
2020-12-30 23:37:35 -0700

    created email signup on frontend

--- simplified-airbnb/src/components/Signup.js
+++ simplified-airbnb/src/components/Signup.js
@@ -4,10 +4,65 @@ import { connect } from 'react-redux'
 import openModal from '../actions/openModal'
 import Login from './Login'
 
+
+const SignupForm = (props) => {
+    return (
+       <div className='sign-up-wrapper'>
+           <div className='col m12'>
+               <div className='input-field' id='email'>
+                   <div className='form-label'>
+                        <input placeholder='Email' onChange={props.changeEmail}/>
+                    </div>
+                </div>
+            </div>
+
+            <div className='col m12'>
+               <div className='input-field' id='password'>
+                   <div className='form-label'>
+                        <input type='password'placeholder='Password' onChange={props.changePassword}/>
+                    </div>
+                </div>
+            </div>
+
+       </div>
+    )
+}
+
 class SignUp extends Component{
+
+    constructor() {
+        super()
+        this.state = {
+        lowerForm: <button type ='button'
+        onClick ={this.showInput}className="sign-up-button">
+            Sign up with email
+        </button>   
+        }
+    }
 
     showLogin = () => {
         this.props.openModal('open', <Login/>)
+    }
+
+    changeEmail = (e) => {
+        this.setState({email:e.target.value})
+    }
+
+    changePassword = (e) => {
+        this.setState({password:e.target.value})
+    }
+
+    showInput = () => {
+        this.setState({
+            lowerForm: <SignupForm 
+                        changeEmail={this.changeEmail} 
+                        changePassword={this.changePassword}/>
+        })
+    }
+
+    submitSignup = (e) => {
+        e.preventDefault()
+        console.log(this.state.email, this.state.password)
     }
 
     render(){
@@ -20,7 +75,7 @@ class SignUp extends Component{
                         <span>or</span>
                         <div className="or-divider"></div>
                     </div>
-                    <button className="sign-up-button">Sign up with email</button>
+                    {this.state.lowerForm}
                     <div className="divider"></div>
                     <div>Already have an account? 
                         <span onClick={this.showLogin} className='signup-loginpage'> Login</span>
