ef31059f655a50cabd3d6b9799d19bca92340c46
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2021-07-08 23:53:33 -0600

    adding spinner for persiste gate load

--- simplified-airbnb/src/index.js
+++ simplified-airbnb/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 
+
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/rootReducer'
@@ -12,6 +13,7 @@ import reduxPromise from 'redux-promise'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // using default web storage
 import { PersistGate } from 'redux-persist/integration/react';
+import Spinner from './utility/Spinner/Spinner';
 
 const persistConfig = {
   key : 'root',
@@ -24,7 +26,7 @@ const presister = persistStore(store)
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate persistor={presister}>
+    <PersistGate loading={<Spinner/>}persistor={presister}>
       <App />
     </PersistGate>
   </Provider>,
