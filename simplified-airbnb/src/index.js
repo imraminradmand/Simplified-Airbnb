054bfa1090c154c9a2ac9a34657545242ece1dc0
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2021-07-08 19:50:48 -0600

    adding redux presist to keep jwt available on refresh

--- simplified-airbnb/src/index.js
+++ simplified-airbnb/src/index.js
@@ -3,16 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import reduxPromise from 'redux-promise'
 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // using default web storage
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = {
+  key : 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 const store = applyMiddleware(reduxPromise)(createStore)(rootReducer)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
