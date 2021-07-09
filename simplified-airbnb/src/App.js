3c25723f675fcbac0a4e9918409c0d913ef6fbd4
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2021-07-09 17:10:37 -0600

    implemented lazy load

--- simplified-airbnb/src/App.js
+++ simplified-airbnb/src/App.js
@@ -1,27 +1,30 @@
-import React, {Component} from 'react'
+import React, { Component, lazy, Suspense } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import SingleFullVenue from './pages/SingleFullVenue/SingleFullVenue'
-import NavBar from './utility/NavBar/NavBar'
-import Modal from './utility/Modal/Modal'
-import CityVenues from './pages/CityVenues/CityVenues'
-import PaymentSuccess from './pages/PaymentSuccess/PaymentSuccess'
-import AccountPage from './pages/AccountPage/AccountPage'
-import Search from './utility/Search/Search'
+import Spinner from './utility/Spinner/Spinner';
 
+const Home = lazy(()=>import ('./pages/Home/Home'))
+const SingleFullVenue = lazy(()=>import ('./pages/SingleFullVenue/SingleFullVenue'))
+const NavBar = lazy(()=>import ('./utility/NavBar/NavBar'))
+const Modal = lazy(()=>import ('./utility/Modal/Modal'))
+const CityVenues = lazy(()=>import ('./pages/CityVenues/CityVenues'))
+const PaymentSuccess = lazy(()=>import ('./pages/PaymentSuccess/PaymentSuccess'))
+const AccountPage = lazy(()=>import ('./pages/AccountPage/AccountPage'))
+const Search = lazy(()=>import ('./utility/Search/Search'))
 class App extends Component {
   render(){
     return(
-      <Router>
+    <Router basename='/fast'>
+      <Suspense fallback={<Spinner/>}>
         <Route path='/' component={NavBar}/>
         <Route exact path='/' component={Home}/>
         <Route exact path='/venue/:vid' component={SingleFullVenue}/>
         <Route exact path='/city/:cityName' component={CityVenues}/>
         <Route exact path="/payment-success/:stripeToken" component={PaymentSuccess} />
         <Route exact path="/account" component={AccountPage} />
-				<Route path="/search/:searchTerm" component={Search} />
+        <Route path="/search/:searchTerm" component={Search} />
         <Route path='/' component={Modal}/>
-      </Router>
+      </Suspense>
+    </Router>
     )
   }
   
